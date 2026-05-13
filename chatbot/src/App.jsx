a68caca9af5dd8307e3ea7a3954b97bb3a57fcbf
Harjot-Singh-dev <harjot.singh.7@outlook.com>
2026-05-13 18:56:34 +0530

    api setup

--- chatbot/src/App.jsx
+++ chatbot/src/App.jsx
@@ -3,12 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import {GoogleGenerativeAi} from "@google/generative-ai";
 
 function App() {
   const [count, setCount] = useState(0)
 
   const apiKey = import.meta.env.VITE_API_GEMINI_KEY;
-  console.timeLog(apiKey)
+  console.log(apiKey)
+
+  // function fetchResponse{
+  //   const ai = new GoogleGenAI({ apiKey: "AIzaSyAJMUc-XWzlN6tdSXizy9vWsYQJLoWmB2Y" });
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+main();
+  
+
 
   return (
     <>
